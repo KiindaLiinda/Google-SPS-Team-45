229e62e9f8f86ba1f336dc4c5ccf9712c80bd851
Linda <thenegativejuan@gmail.com>
2021-03-31 20:00:32 -0700

    add initial maps to LA using JS/TS and moced async script link

--- maps.js
+++ maps.js
@@ -1,1 +1,10 @@
 //This will include all the raw code for the Google Maps API
+let map;
+
+//Create the intial map to view the Greater Los Angeles Area
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 34.0522, lng: 118.2437 },
+    zoom: 8,
+  });
+}
