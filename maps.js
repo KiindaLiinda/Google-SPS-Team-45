d319f9f3ee5dc7daae39a2a31be658175a68ffec
johnatan <garciajohnatan151@gmail.com>
2021-04-02 11:54:51 -0600

    inserted a small map for testing purposes

---
+++ maps.js
@@ -0,0 +1,9 @@
+//This will include all the raw code for the Google Maps API
+let map;
+
+//Create the intial map to view the Greater Los Angeles Area
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 34.0361, lng: -118.2437 }, zoom: 8,
+  });
+}
