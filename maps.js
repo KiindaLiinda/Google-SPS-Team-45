264c96207e6a082cba3412cf1d21675503f413dd
johnatan <garciajohnatan151@gmail.com>
2021-04-03 10:24:24 -0600

    added the map to the html without any markers

--- maps.js
+++ maps.js
@@ -4,6 +4,6 @@ let map;
 //Create the intial map to view the Greater Los Angeles Area
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 34.0361, lng: -118.2437 }, zoom: 8,
+    center: { lat: 34.0361, lng: -118.2437 }, zoom: 10,
   });
 }
