e1e641c589cc79dd10bb324ea5dcf5eef0d51bad
johnatan <garciajohnatan151@gmail.com>
2021-04-04 13:06:25 -0600

    so I followed week 3 walkthrough on marker but i do not see them im pretty sure i need the ts file to continue so im im just to commit this i also added 2 new location in the json for testing other than that im going to try to get the ts file so i could test them out

---
+++ maps.ts
@@ -0,0 +1,10 @@
+//This is the Goggle Maps API code for typescript
+let map: google.maps.Map;
+
+//Set Initial Map to the Greater Los Angeles Area
+function initMap(): void {
+  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    center: { lat: 34.0522, lng: 118.2437 },
+    zoom: 8,
+  });
+}
