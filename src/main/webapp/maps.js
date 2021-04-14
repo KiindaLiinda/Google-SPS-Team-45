0d058061091ea519027ecd46e3b8463d620789c1
Nicolas Noriega <nicolasn8171@gmail.com>
2021-04-14 10:09:34 -0700

    Pulled code from main branch and added changes made to index.html and style.css, as well as maps.js in order to display a new dropdown button and added in markers to test

--- src/main/webapp/maps.js
+++ src/main/webapp/maps.js
@@ -1,18 +1,46 @@
 //pulls the info from the resources.json file
 fetch("./resources.json")
-.then(response => {
-  return response.json();
-})
-.then(data => console.log(data));
+    .then(response => {
+        return response.json();
+    })
+    .then(data => console.log(data));
 
 //This will include all the raw code for the Google Maps API
 let map;
 
+
 //Create the intial map to view the Greater Los Angeles Area
 function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-    center = {lat: 34.0361, lng: -118.2437},
-     zoom: 10,
-  });
-}
 
+    var map = new google.maps.Map(document.getElementById("map"),
+        {
+            center : {lat: 34.0361, lng: -118.2437},
+            zoom: 9,
+        });
+
+    var bridgeToHome = {lat: 34.411760, lng: -118.538130}
+    var hathaway = {lat: 34.196210, lng: -118.165970};
+    var SRO = {lat: 34.044110, lng: -118.245330};
+
+    var BTHmarker = new google.maps.Marker({
+        position: bridgeToHome,
+        map: map,
+        title: 'Bridge to Home Shelter'
+
+    });
+
+    var Hathaway_marker = new google.maps.Marker({
+        position: hathaway,
+        map: map,
+        title: 'Hathaway-Sycamores'
+
+    });
+
+    var SRO_marker = new google.maps.Marker({
+        position: SRO,
+        map: map,
+        title: 'SRO Housing Corporation'
+
+    });
+
+}
--- src/main/webapp/parseJSON.java
+++ src/main/webapp/parseJSON.java
@@ -1,12 +1,17 @@
-import java.io.FileReader;
-import java.net.URI;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.Map;
-
-import org.json.*;
-import org.json.simple.parser.JSONParser;
+import java.util.logging.Logger;
 
 
 //HOW TO INSTALL JAR FOR JSON PARSING: https://www.geeksforgeeks.org/parse-json-java/
@@ -14,17 +19,116 @@ import org.json.simple.parser.JSONParser;
 public class parseJSON
 {
 
-    public static void main(String[] args) throws Exception
+    public final static Logger log = Logger.getLogger("parseJSON");
+
+    private static parseJSON instance = null;
+
+    private JSONParser jsonParser;
+
+    public parseJSON()
     {
+        jsonParser = new JSONParser();
+    }
+
+    public static parseJSON getInstance()
+    {
+        if (instance == null) {
+            instance = new parseJSON();
+        }
+        return instance;
+    }
 
 
-        String jsonString = new String(Files.readAllBytes(Paths.get("src/resources.json")));
+    private String getRequest(String url) throws Exception {
 
-        //JSONTokener tokener = new JSONTokener(uri.toURL().openStream());
-        //JSONObject jsonObject = new JSONObject(tokener);
+        final URL obj = new URL(url);
+        final HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        con.setRequestMethod("GET");
+
+        if (con.getResponseCode() != 200) {
+            return null;
+        }
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        return response.toString();
+    }
 
 
-        JSONObject jsonObject = new JSONObject(jsonString);
+
+    public Map<String, Double> getCoordinates(String address) throws Exception {
+        Map<String, Double> result;
+
+        StringBuffer query;
+        String[] split = address.split(" ");
+        String queryResult = null;
+
+        query = new StringBuffer();
+        result = new HashMap<String, Double>();
+
+        query.append("http://nominatim.openstreetmap.org/search?q=");
+
+        if(split.length == 0)
+        {
+            return null;
+        }
+
+        for (int i = 0; i < split.length; i++) {
+            query.append(split[i]);
+            if (i < (split.length - 1)) {
+                query.append("+");
+            }
+        }
+        query.append("&format=json&addressdetails=1");
+
+        //log.debug("Query:" + query);
+
+        /*
+        try {
+            queryResult = getRequest(query.toString());
+        } catch (Exception e) {
+            log.error("Error when trying to get data with the following query " + query);
+        }
+
+         */
+
+        queryResult = getRequest(query.toString());
+
+
+
+        Object obj = JSONValue.parse(queryResult);
+        //log.debug("obj=" + obj);
+
+        if (obj instanceof JSONArray) {
+            JSONArray array = (JSONArray) obj;
+            if (array.length() > 0) {
+                JSONObject jsonObject = (JSONObject) array.get(0);
+
+                String lon = (String) jsonObject.get("lon");
+                String lat = (String) jsonObject.get("lat");
+
+                result.put("lon", Double.parseDouble(lon));
+                result.put("lat", Double.parseDouble(lat));
+
+            }
+        }
+
+        return result;
+
+    }
+
+
+
+    private static void parseFile(String jsonFile) throws Exception {
+        JSONObject jsonObject = new JSONObject(jsonFile);
 
 
         //JSONObject jo = new JSONObject();
@@ -34,11 +138,24 @@ public class parseJSON
         for(int i = 0; i < jsonArray.length(); i++)
         {
             String place_name = jsonArray.getJSONObject(i).getString("Address");
-            System.out.println(place_name);
-        }
+            System.out.println("Address: " + place_name);
 
+            Map<String, Double> coordinates;
+            coordinates = parseJSON.getInstance().getCoordinates(place_name);
+
+            System.out.println("Lat: " + coordinates.get("lat") + ", Long: " + coordinates.get("lon"));
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception
+    {
+        String jsonString = new String(Files.readAllBytes(Paths.get("src/main/java/resources.json")));
+
+        parseFile(jsonString);
 
     }
+
 
 }
 
